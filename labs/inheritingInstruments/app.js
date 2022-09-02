5161f43fc5f0a2551420fb2d6debf4ab719792dc
harremar <78804575+harremar@users.noreply.github.com>
2022-09-02 15:02:13 -0400

    com

--- labs/inheritingInstruments/app.js
+++ labs/inheritingInstruments/app.js
@@ -1,44 +1,50 @@
-let instruments = [];
+let instruments = []; //empty array
+//where you get sound
 let synthApp = {
   synth: new Tone.Synth().toDestination(),
-  notes: ["C4", "E4", "G4"],
-  currentInstrument: 0,
+  notes: ["C4", "E4", "G4"], //the sould each instrument makes
+  currentInstrument: 0, //start at 0
+  //function that plays sound
   playSound() {
-    this.synth.triggerAttackRelease(this.notes[this.currentInstrument], "8n");
-    this.currentInstrument++;
-    // this.activatedCallback(); //when a instrument plays a note
+    this.synth.triggerAttackRelease(this.notes[this.currentInstrument], "8n"); // plays a eighth note
+    this.currentInstrument++; //go to next instrument
     console.log(this.currentInstrument);
+    //play string note
     if (this.currentInstrument === 3) {
       this.currentInstrument = 0;
       instruments[2] = new Strings("quite");
-      instruments[2].playNext();
+      instruments[2].playNext(); //this gets message
     }
+    //play percussion note
     if (this.currentInstrument === 1) {
       instruments[1] = new Percussion("low");
       instruments[1].playNext();
     }
+    //play woodwinds note
     if (this.currentInstrument === 2) {
       instruments[0] = new Woodwinds("very loud");
       instruments[0].playNext();
     }
+    //wait 0.5sec
     setTimeout(this.playSound.bind(this), 500);
   },
 };
 
+//base class
 class Instrument {
   constructor(loudness, family, verb) {
     this.loudness = loudness;
     this.family = family;
     this.verb = verb;
   }
+  //function creates sentence
   playNext() {
     console.log(
       this.family + " " + this.verb + " at a " + this.loudness + " volume"
     );
-
-    // setTimeout(instruments[0].musicPlaying, 500);
   }
 }
+//child classes
 class Woodwinds extends Instrument {
   //family and verb in child constructor
   constructor(loudness) {
